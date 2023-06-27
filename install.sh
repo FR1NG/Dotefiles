3c491fb32edd785ccde61d7635a9f9b3b1236eb4
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-06-27 12:27:31 +0100

    making a list of packages needed to be installed on fresh arch installation

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# thi is the list of dependencies i need
+# greetd : the login manager
+# lf: terminal based file manager
+# nvim: ghaniy 3an ta3riif
+# waybar: the system bar
+# sway: window manager
+# alacritty: terminal emulator
+# blueman-manager: gui bluetooth manager
+# fzf: fuzzy finder
+# swaylock: lock manager
+# qutebrowser: the best of the best minimal, configurable, vimlike browser
+# wofi: application luncher
+# mvp: media player
+# spotify-player: terminal based spotify player
+# axel: download manager
+# slurp: waylan compositor selection utility
+# stow
+# zsh
+# prezto
+# wl-clipboard
+
+# TODO
+# automate the installation of the font
